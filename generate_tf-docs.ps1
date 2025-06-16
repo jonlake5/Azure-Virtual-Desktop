972a347309f24570c80e970400dbb8211617252c
Jonathan Lake <jonlake5@gmail.com>
2025-06-16 10:30:49 -0400

    Comment removal of tfdoc generation script

--- generate_tf-docs.ps1
+++ generate_tf-docs.ps1
@@ -1,6 +1,4 @@
 foreach ($directory in (get-childitem -path "./modules" -Directory)) {
-    # set-location -Path $directory.FullName
     terraform-docs.exe markdown table --output-file README.md --output-mode inject $directory
-    # write-output $directory.FullName
 }
 terraform-docs.exe markdown table --output-file README.md --output-mode inject "./"
